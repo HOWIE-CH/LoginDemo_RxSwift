5347db0b1e3f4f3c90b408fd1909251cc6e78813
HOWIE-CH <zh200742zh@126.com>
2017-03-01 23:32:57 +0800

    - demo 基本完成

--- loginRx/Classes/Home/View/HomeViewController.swift
+++ loginRx/Classes/Home/View/HomeViewController.swift
@@ -16,6 +16,7 @@ class HomeViewController: UIViewController {
     fileprivate lazy var tableView = { () -> UITableView in 
         let tabelView = UITableView(frame: CGRect(x: 0, y: 108, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - 108),
                                     style: .plain)
+        tabelView.rowHeight = 66
         return tabelView
     }()
     
@@ -26,27 +27,72 @@ class HomeViewController: UIViewController {
     
     let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, HerosItem>>()
 
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(searchView)
         view.addSubview(tableView)
+        
+        
         
         dataSource.configureCell = { (_, tableView, indexPath, item) in
             var cell = tableView.dequeueReusableCell(withIdentifier: "herosCell")
             if cell == nil {
                 cell = UITableViewCell(style: .subtitle, reuseIdentifier: "herosCell")
             }
+            cell!.imageView?.image = UIImage(named: item.icon)
+            cell!.textLabel?.text = item.name
+            cell!.detailTextLabel?.text = item.intro
             return cell!
         }
         
         
+        let homeViewMode = HomeViewModel()
         
         
         
+        searchView.rx.text.orEmpty
+            .bindTo(homeViewMode.searchText)
+            .addDisposableTo(disposeBag)
         
         
+        homeViewMode.getSearchResult()
+            .bindTo(tableView.rx.items(dataSource: dataSource))
+            .addDisposableTo(disposeBag)
         
+        tableView.rx
+            .setDelegate(self)
+            .addDisposableTo(disposeBag)
+        
+        tableView.rx.itemSelected
+            .map { [weak self] indexPath in
+                return (indexPath, self?.dataSource[indexPath])
+            }
+            .subscribe(onNext: {(indexPath, item) in
+                self.showAlter(item: item)
+            })
+            .addDisposableTo(disposeBag)
     }
 
 
 }
+
+extension HomeViewController: UITableViewDelegate {
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        view.endEditing(true)
+    }
+}
+extension HomeViewController {
+    
+    func showAlter(item: HerosItem?) {
+        let alterVc = UIAlertController(title: "Demo结束了！", message: item?.intro ?? "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "好吧", style: .default, handler: nil)
+        alterVc.addAction(action)
+        present(alterVc, animated: true, completion: nil)
+    }
+    
+    
+}
+
--- loginRx/Classes/Home/ViewModel/HomeViewModel.swift
+++ loginRx/Classes/Home/ViewModel/HomeViewModel.swift
@@ -12,8 +12,9 @@ import RxDataSources
 
 class HomeViewModel {
     
+    var searchText = Variable<String>("")
+
     lazy var herosArray = { () -> [HerosItem] in
-        
         guard let filePath = Bundle.main.path(forResource: "heros.plist", ofType: nil) else {
             return [HerosItem]()
         }
@@ -22,28 +23,47 @@ class HomeViewModel {
             return [HerosItem]()
         }
         
-        var herosArray = [HerosItem]()
+        let tmpArray = NSMutableArray()
         for  dict in allArray {
-            var hero = HerosItem(dict: dict)
-            (herosArray as NSArray).adding(hero)
+            let hero = HerosItem(dict: dict)
+//            herosArray.append(hero)
+                tmpArray.add(hero)
         }
-        return herosArray
+        let array: NSArray = tmpArray
+        return array as! [HerosItem]
+
     }()
-    var searchText = Variable<String>("")
-    var searchResult: Observable<[SectionModel<String, HerosItem>]>
     
-    init() {
-        searchResult = searchText.asObservable()
-            .map { (keyWord) -> Observable<[SectionModel<String, HerosItem>]> in
+    
+    
+    func getSearchResult() -> Observable<[SectionModel<String, HerosItem>]> {
+        
+        let result: Observable<[SectionModel<String, HerosItem>]> = searchText.asObservable()
+            .flatMapLatest{ keyWord -> Observable<[SectionModel<String, HerosItem>]> in
+                print(#file, #line, #function, ":++++++++++")
                 
+                // 默认搜索字为空时候加载所有数据
+                if keyWord.characters.count == 0 {
+                    let section = SectionModel(model: "", items: self.herosArray)
+                    return Observable.just([section])
+                }
+                // 根据关键字搜索
+                var resultArray = [HerosItem]()
                 
+                for item in self.herosArray {
+                    if item.name.contains(keyWord) {
+                        resultArray.append(item)
+                    }
+                }
                 
-                
-                return
-                
+                let section = SectionModel(model: "", items: resultArray)
+                return Observable.just([section])
             }
+            .shareReplay(1)
+        
+        return result
         
     }
     
-}
+   }
 
